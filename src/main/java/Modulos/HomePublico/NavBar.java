44252344e78d3882a396122a01145ba39c076857
Javier Avalos <jaivalos.dev@gmail.com>
2024-12-10 11:30:02 -0600

    Home Publico - NavBar - Idiomas

--- src/main/java/Modulos/HomePublico/NavBar.java
+++ src/main/java/Modulos/HomePublico/NavBar.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.interactions.Actions;
 
 import java.time.Duration;
 import java.util.ArrayList;
@@ -38,6 +39,7 @@ public class NavBar {
         String btnProductosXpath = "/html/body/div[1]/div[2]/ul/li[1]/a";
         String btnAgenciaXpath = "/html/body/div[1]/div[2]/ul/li[2]/a";
         String btnHorariosXpath = "/html/body/div[1]/div[2]/ul/li[3]/a";
+        String btnIdiomaXpath = "//*[@id=\"menu_gral\"]/ul/li/a";
 
         String currentUrl = "";
 
@@ -132,6 +134,45 @@ public class NavBar {
             }
 
             driver.navigate().back();
+
+
+            //Lista Idioma
+            WebElement btnIdioma = driver.findElement(By.xpath(btnIdiomaXpath));
+
+            // Usa la clase Actions para hacer hover sobre el título
+            Actions actions = new Actions(driver);
+            actions.moveToElement(btnIdioma).perform();
+
+            System.out.println("BTN Idioma Hover ✓");
+
+            try{
+                // Validar la opción Español
+                WebElement opcionEspanol = driver.findElement(By.cssSelector("#menu_gral > ul > li > ul > li:nth-child(1) > a"));
+                if (opcionEspanol.isDisplayed() && opcionEspanol.getText().equalsIgnoreCase("Español")) {
+                    System.out.println("La opción 'Español' está visible y es correcta.");
+                    test.log(Status.INFO, "La opción 'Español' está visible y es correcta.");
+                } else {
+                    System.out.println("La opción 'Español' no está visible o es incorrecta.");
+                    test.log(Status.INFO, "La opción 'Español' no está visible o es incorrecta.");
+                }
+
+                // Validar la opción Inglés
+                WebElement opcionIngles = driver.findElement(By.cssSelector("#menu_gral > ul > li > ul > li:nth-child(2) > a"));
+                if (opcionIngles.isDisplayed() && opcionIngles.getText().equalsIgnoreCase("English")) {
+                    System.out.println("La opción 'English' está visible y es correcta.");
+                    test.log(Status.INFO, "La opción 'English' está visible y es correcta.");
+                } else {
+                    System.out.println("La opción 'English' no está visible o es incorrecta.");
+                    test.log(Status.INFO, "La opción 'English' no está visible o es incorrecta.");
+                }
+                Thread.sleep(3000);
+
+                test.log(Status.INFO, "BTN Idioma funcional");
+
+            }catch (Exception e){
+                test.log(Status.FAIL, "No se muestran las opciones al hacer Hover: " + e);
+            }
+
 
         }catch (Exception e){
             test.log(Status.FAIL, e);
